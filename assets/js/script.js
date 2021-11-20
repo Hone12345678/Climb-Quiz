5115f816bfb61a6278cd9ea5f816de5b3b824d88
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-11-19 20:27:11 -0700

    building start quiz button

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+// select the start button
+var startQuizBtnEl = document.querySelector("#startQuizBtn");
+
+// Attaching event listener to increment button element
+startQuizBtnEl.addEventListener("click", function() {
+    console.log("test")
+});
